bb86bfe6640bbc8018a466fcae5e8a03817c6983
Rohan Mishra <rohanmishra2017@gmail.com>
2018-01-20 20:25:36 -0800

    Adding basic files of the blockchain

---
+++ simple_node.py
@@ -0,0 +1,15 @@
+# -*- encoding: utf-8 -*-
+"""
+simple_node.py
+
+- Defines a node in the simple blockchain
+    + Can create a blockchain if none exists
+"""
+
+from blockchain import Block
+from blockchain import Blockchain
+
+class Node:
+    
+    
+
