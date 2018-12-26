99cec3c6f7f73f270c79bfdadd1eb0e350294c1f
Meeshbhoombah <rohanmishra2017@gmail.com>
2018-12-26 04:50:17 -0500

    Added /requestValidation

--- app.js
+++ app.js
@@ -17,19 +17,72 @@ app.use(bodyParser.urlencoded({ extended: true }));
 const chain = new Chain.Blockchain();
 
 
-/* READ validation */
-app.post('/requestValidation', (req, res) => {
+const VALIDATION_TIMEOUT = 300; // (seconds)
+var cache = {};
 
+
+/******** REQUEST MESSAGE VALIDATION *********/
+const addressSchema = yup.object().shape({
+    address: yup.string().max(34).required()
+})
+
+app.post('/requestValidation', (req, res) => {
+    let requestAddress = req.body;
+
+    addressSchema
+    .validate(requestAddress)
+    .catch((err) => {
+        let response = {
+            statusCode: 400,
+            error: err
+        };
+
+        return res.status(response.statusCode).send(response);
+    });
+
+
+    let address = requestAddress['address'];
+    let requestObject = cache[address];
+    let timestamp = new Date().getTime().toString().slice(0, -3);
+    
+    if (requestObject) {
+        let elapsedTime = timestamp - requestObject.timestamp;
+        let refreshedWindow = 300 - elapsedTime;
+
+        requestObject.timestamp = refreshedWindow;
+        cache[address] = requestObject;
+
+        return res.status(200)        
+            .set("Content-Type", "application/json")
+            .send(cache[address]);
+    }
+
+
+    let response = {
+        walletAddress: address,
+        requestTimeStamp: timestamp,
+        message: `${address}:${timestamp}:starRegistry`,
+        validationWindow: 300
+    }
+
+    cache[address] = response;
+    let timeout = response.validationWindow * 1000;
+    setTimeout(() => { delete requestCache[userResponse.address]; }, timeout);
+
+    return res.status(200)
+        .set("Content-Type", "application/json") 
+        .send(response);
 });
 
 
+/******** VALIDATE MESSAGE SIGNATURE *********/
 app.post('/message-signature/validate', (req, res) => {
 
 });
 
 
 /* CREATE star */
-let starBodySchema = yup.object().shape({
+const starBodySchema = yup.object().shape({
     address: yup.string().max(34).required(),
     star: yup.object().shape({
         dec: yup.string().min(3).required(),
