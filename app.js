80aa444a524a8692a4e1b4e3bbd4cb306159fbc9
Meeshbhoombah <rohanmishra2017@gmail.com>
2018-12-26 02:38:10 -0500

    Added all routes

--- app.js
+++ app.js
@@ -1,28 +1,74 @@
 
-const express    = require('express');
-const bodyParser = require('body-parser')
-const Chain     = require('./simple/Blockchain');
-const Block     = require('./simple/Block');
+const express     = require('express');
+const bodyParser  = require('body-parser')
+const Chain       = require('./simple/Blockchain');
+const Block       = require('./simple/Block');
+const yup         = require('yup');
 
 
 app = express();
 const PORT = 8000;
 
 
-let chain = new Chain.Blockchain();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 
-app.get('/block/:height', function(req, res) {
-    console.log(req.params['height']);
+const chain = new Chain.Blockchain();
+
+
+/* READ validation */
+app.post('/requestValidation', (req, res) => {
+
 });
 
 
-app.post('/block', function(req, res) {
-    console.log(req.body);
+app.post('/message-signature/validate', (req, res) => {
+
 });
 
 
-app.listen(PORT, function() {
-    console.log('Listening on port:', PORT);
+/* CREATE star */
+let starBodySchema = yup.object().shape({
+    address: yup.string().max(34).required(),
+    star: yup.object().shape({
+        dec: yup.string().min(3).required(),
+        re: yup.string().min(3).required(),
+        story: yup.string().matches(/[a-zA-Z0-9]/).min(3).required()
+    })
 });
+
+
+app.post('/block', (req, res) => {
+
+});
+
+
+/* READ one block from hash */
+app.get('/stars/:hash', (req, res) => {
+
+});
+
+
+/* READ one decoded star with address */
+app.get('/stars/:address', (req, res) => {
+
+});
+
+
+/* READ one block from height */
+app.get('/block/:height', async (req, res) => {
+    var blockRef = parseInt(req.params['height']);
+
+    await chain.getBlock(blockRef)
+    .then((block) => {
+             
+    })
+    .catch((err) => {
+        console.log('Could not retrive Block:', err.stack);
+    })
+});
+
+
+app.listen(PORT, () => console.log('Listening on port:', PORT));
 
